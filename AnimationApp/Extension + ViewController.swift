ce0ed3f76c37d629dcef7ab45224147caa3cad5d
Gr1ngoire <gringoirelamusicart@gmail.com>
2021-05-09 19:52:09 +0300

    The task is complete

---
+++ AnimationApp/Extension + ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  Extension + ViewController.swift
+//  AnimationApp
+//
+//  Created by admin on 09.05.2021.
+//
+
+import Spring
+
+
+// Extension with the function to set parameters for our animation
+extension ViewController {
+    
+    func giveParams(target:SpringView, infoOutput:UILabel,  duration:CGFloat = 1, scaleX:CGFloat = 1, scaleY:CGFloat = 1) {
+        target.duration = duration
+        target.scaleX = scaleX
+        target.scaleY = scaleY
+        
+        
+        infoOutput.text = "The parameters are duration: \(duration), scaleX: \(scaleX), scaleY: \(scaleY)"
+    }
+}
--- AnimationApp/ViewController.swift
+++ AnimationApp/ViewController.swift
@@ -5,15 +5,67 @@
 //  Created by admin on 08.05.2021.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    @IBOutlet weak var mainView: SpringView!
+    @IBOutlet weak var mainButton: SpringButton!
+    @IBOutlet weak var infoOuputLabel: UILabel!
+
+    // animations array
+    private let animations = [
+        "pop",
+        "squeeze",
+        "swing",
+        "flipY",
+        "wobble",
+        "zoomIn"
+    ]
+    
+    // animation index
+    private var animIndex = 0
+    
+    @IBAction func startViewAnimation(_ sender: SpringButton) {
+        
+        // adding animation for mainView and mainButton
+        mainView.animation = animations[animIndex]
+        mainButton.animation = animations[animIndex]
+        
+        // setting parameters for the mainView animation according to the situation
+        switch animations[animIndex] {
+        case "pop":
+            giveParams(target: mainView, infoOutput: infoOuputLabel, duration: 3, scaleX: 1.5, scaleY: 1.5)
+        case "squeeze":
+            giveParams(target: mainView, infoOutput: infoOuputLabel, duration: 2, scaleX: 1.25, scaleY: 1.25)
+        case "swing":
+            giveParams(target: mainView, infoOutput: infoOuputLabel, duration: 1.6, scaleX: 1, scaleY: 1)
+        case "flipY":
+            giveParams(target: mainView, infoOutput: infoOuputLabel, duration: 1, scaleX: 1, scaleY: 1)
+        case "wobble":
+            giveParams(target: mainView, infoOutput: infoOuputLabel, duration: 4, scaleX: 1.25, scaleY: 1.25)
+        case "zoomIn":
+            giveParams(target: mainView, infoOutput: infoOuputLabel, duration: 4.5, scaleX: 1.4, scaleY: 1.4)
+        default:
+            giveParams(target: mainView, infoOutput: infoOuputLabel)
+        }
+        
+        // index error check up
+        if animIndex < animations.count - 1 {
+            animIndex += 1
+        } else {
+            animIndex = 0
+        }
+        
+        // next animation name
+        mainButton.setTitle("Next: \(animations[animIndex])", for: .normal)
+        
+        // animating mainView and mainButton
+        mainView.animate()
+        mainButton.animate()
+    }
+    
 
 }
 
